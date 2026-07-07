d76b35b08206892e15107a15745bdabb5ff6331d
Wellz <owellingtondepaula@gmail.com>
2026-07-07 15:34:26 -0300

    feat(projeto): Implementar funcionalidade de login
    Adiciona sistema de autenticação ao mini-blog com formulário de login, validação de dados e template correspondente. Inclui renomeação das classes de formulários para seguir convenção de nomenclatura (Form suffix) e integração com Flask-Login para gerenciamento de sessões de usuário.

--- projetos/mini-blog/forms.py
+++ projetos/mini-blog/forms.py
@@ -1,16 +1,18 @@
-from wtforms import IntegerField, StringField, EmailField, PasswordField
+from wtforms import IntegerField, StringField, EmailField, PasswordField, SubmitField
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, EqualTo, InputRequired
 
-class UserRegister(FlaskForm):
+class UserRegisterForm(FlaskForm):
     id = IntegerField("Id", validators=[InputRequired(message='User must have a ID.')])
     username = StringField("Username", validators=[DataRequired(message='User must have a username.')])
     email = EmailField("Email", validators=[DataRequired(message='User must have a email.')])
     password = PasswordField("Password", validators=[DataRequired(message='User must have a password.')])
     confirm_password = PasswordField("Confirm Password", validators=[EqualTo('password', message='Passwords must be the same.')])
+    submit = SubmitField('Submit')
 
-class UserLogin(FlaskForm):
+class UserLoginForm(FlaskForm):
     id = IntegerField("Id", validators=[InputRequired(message='User must have a ID.')])
     username = StringField("Username", validators=[DataRequired(message='User must have a username.')])
     email = EmailField("Email", validators=[DataRequired(message='User must have a email.')])
     password = PasswordField("Password", validators=[DataRequired(message='User must have a password.')])
+    submit = SubmitField('Submit')
--- projetos/mini-blog/routes.py
+++ projetos/mini-blog/routes.py
@@ -1,7 +1,19 @@
 from flask import render_template, redirect, url_for, flash
 from models import User
+from forms import UserRegisterForm, UserLoginForm
+from flask_login import login_user, logout_user, login_required, current_user
 from app import app, db
 
 @app.route('/')
 def index():
+    
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    form = UserLoginForm()
+    
+    if (form.validate_on_submit()):
+        pass
+    
+    return render_template('login.html', form=form)
